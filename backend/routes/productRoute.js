333b0fc8b5c3474178b9b41d5bf9d8a3b324d2ca
Phuuoc <phuocntgcc18019@fpt.edu.vn>
2022-03-19 20:14:45 +0700

    second commit

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getAllProducts,createProduct, updateProduct, deleteProduct, getProductDetails, createProductReview } = require("../controllers/productController");
+const { getAllProducts,createProduct, updateProduct, deleteProduct, getProductDetails, createProductReview, getProductReviews, deleteReview } = require("../controllers/productController");
 const { isAuthenticatedUser, authorizeRoles} = require("../middleware/auth");
 
 const router = express.Router();
@@ -16,5 +16,7 @@ router
 router.route("/product/:id").get(getProductDetails);
 
 router.route("/review").put( isAuthenticatedUser, createProductReview);
+
+router.route("/reviews").get(getProductReviews).delete(isAuthenticatedUser, deleteReview);
 
 module.exports = router;
